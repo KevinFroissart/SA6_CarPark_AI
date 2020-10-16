4ae8cd78ee5bbec785a0aeaed98fb055f484dee9
Froissart Kévin <kevin.froissart69@gmail.com>
2020-10-16 14:21:39 +0200

    un peu de doc et tests sur le main

--- SA6/src/main.cpp
+++ SA6/src/main.cpp
@@ -19,21 +19,31 @@ void connexionServer(Voiture * v, int port){
 int main (void){
 
     //int nb_parking = 2;
-    Parking * P2 = new Parking(1, "parking.csv");
+    Parking * P1 = new Parking(1, "parking.csv");
+    Parking * P2 = new Parking(2, "parking.csv");
     
     Voiture * v1 = new Voiture(8, "voitures.csv");
     Voiture * v2 = new Voiture(12, "voitures.csv");
+    Voiture * v3 = new Voiture(18, "voitures.csv");
 
-    thread * t1 = new thread(startServer, P2); 
-    t1->detach();
+    thread * tp1 = new thread(startServer, P1); 
+    thread * tp2 = new thread(startServer, P2);
 
-    sleep(2);
+    tp1->detach();
+    tp2->detach();
 
-    thread * t2 = new thread(connexionServer, v1, 8080);
-    thread * t3 = new thread(connexionServer, v2, 8080);
+    sleep(3);
+    //Alternative au sleep ? ..
 
-    t2->join();
-    t3->join();
+    thread * tv1 = new thread(connexionServer, v1, 8080); // EN théorie on récupère le port du server depuis getPort() dans le parking
+                                                          // On peux donc imaginer un while(!parkingTrouve) puis on puise dans une liste de parking
+                                                          // et l'on se connecte tant qu'on ne trouve pas un accord. 
+    thread * tv2 = new thread(connexionServer, v2, 8080); 
+    thread * tv3 = new thread(connexionServer, v3, 8081);
+
+    tv1->join();
+    tv2->join();
+    tv3->join();
 
     // cout<<"id du thread : "<<t1.get_id()<<endl;
     //Parking * P3 = new Parking(2, "parking.csv");
@@ -56,11 +66,14 @@ int main (void){
         tab_threads_parking[i].join();
     }*/
 
-    delete t1;
-    delete t2;
-    delete t3;
+    delete tp1;
+    delete tp2;
+    delete tv1;
+    delete tv2;
+    delete tv3;
     delete v1;
     delete v2;
+    delete P1;
     delete P2;
     return 0;
 }
