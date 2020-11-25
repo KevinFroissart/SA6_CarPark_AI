04cb483d9e6a1d5404b7fb47cdcb8aaa66e31f14
Froissart Kevin <kevin.froissart69@gmail.com>
2020-11-25 21:37:11 +0100

    add progress bar fonctionnelle

--- SA6/src/main.cpp
+++ SA6/src/main.cpp
@@ -1,6 +1,7 @@
 #include "../headers/Voiture.hpp"
 #include "../headers/Parking.hpp"
 #include "../headers/ToolBox.hpp"
+#include "../headers/main_back.hpp"
 #include <SFML/Graphics.hpp>
 #include <string.h>
 #include <thread>
@@ -9,43 +10,20 @@
 #include <time.h>
 using namespace std;
 
-void startServer(Parking * P){
-    if(!P->demarerServer()) cout << "Le parking ne parviens pas à démarrer le serveur" << endl;
-}
-
-void connexionServer(Voiture * v, int port, int id){
-    if(!v->connexionServer(port, id)) cout << "La voiture ne parviens pas à se connecter au serveur" << endl;
+void back_end_process(Main_back * mb){
+    mb->process();
 }
 
 int main (void){
 
-    int nb_parking = 3;
-    vector<Parking *> listeParking;
-    vector<thread *> listeThreadParking;
+    Main_back * main_b = new Main_back();
 
-    for(int i = 0; i < nb_parking; i++){
-        listeParking.push_back(new Parking(i+1, "parking.csv"));  
-        listeThreadParking.push_back(new thread(startServer, listeParking[i]));
-        listeThreadParking[i]->detach();
-    }
+    thread * back_thread = new thread(back_end_process, main_b);
+    back_thread->detach();
 
-    sleep(4);
-
-    int nb_voiture = 30;
-    vector<Voiture *> listeVoiture;
-    vector<Voiture *> listeVoitureTmp;
-
-    for(int i = 0; i < nb_voiture; i++){
-        listeVoitureTmp.push_back(new Voiture(i + 1, "voitures.csv"));
-    }
-
-    //Ajouter des voitures de manière aléatoire dans notre liste
-    while(listeVoiture.size() != (unsigned int) nb_voiture){
-        srand(time(NULL));
-        int voitureRand = rand() % (int) listeVoitureTmp.size();
-        
-        listeVoiture.push_back(new Voiture(listeVoitureTmp[voitureRand]->getID(), "voitures.csv"));
-        listeVoitureTmp.erase(listeVoitureTmp.begin() + voitureRand);
+    int tab_capacite[main_b->m_nbParking];
+    for(int i = 0; i < main_b->m_nbParking; i++){
+        tab_capacite[i] = stoi(main_b->m_listeParking[i]->getCapaciteTotale());
     }
 
     sf::RenderWindow window;
@@ -54,74 +32,66 @@ int main (void){
     sf::Event event;
 
     while(window.isOpen()){
+        bool closed = false;
         while(window.pollEvent(event)){
 
             switch (event.type){
-                case sf::Event::Closed: 
-                    window.close(); 
+                case sf::Event::Closed:
+                    window.close();
+                    main_b->~Main_back(); 
+                    closed = true;
                     break;
                 case sf::Event::KeyPressed :
-                    if(event.key.code == sf::Keyboard::Q) window.close();
+                    if(event.key.code == sf::Keyboard::Q){
+                        window.close();  
+                        main_b->~Main_back();
+                        closed = true;
+                    } 
                     break;
                 default:
                     break;
             }
         }
-        /*
-        //Creer les threads pour chaque voitures
-        for(int i = 0; i < nb_voiture; i++){
-            for(int j = 0; j < nb_parking && listeVoiture[i]->rechercheParking; j++){
-                thread * tmp = new thread(connexionServer, listeVoiture[i], listeParking[j]->getPort(), listeParking[j]->getId());
-                tmp->join();
-                delete tmp;
-            }
-            cout << (listeVoiture[i]->rechercheParking ? "La voiture n'as pas trouvé de parking" : "La voiture a trouvé un parking");
-            cout << endl;
-        }
 
-        for(int i = 0; i < nb_parking; i++){
-            delete listeThreadParking[i];
-            delete listeParking[i];
-        }      
-        for(Voiture* voiture : listeVoiture){
-            delete voiture;
-        }*/
-
-        sf::Font font;
-        if (!font.loadFromFile("arial.ttf"))
-            return EXIT_FAILURE;
-        
-        window.clear();
-
-        for(int i = 0; i < nb_parking; i++){
-            sf::Text text("Parking " + to_string(i+1), font, 50);
-            int y = i*50;
-            text.move(0.f, y);
+        if(!closed){
             
-            int length = 300;
-            int width = 50;
-            float progress_scale = length/stoi(listeParking[i]->getCapaciteTotale());
+            sf::Font font;
+            if (!font.loadFromFile("arial.ttf"))
+                return EXIT_FAILURE;
+            
+            window.clear();
 
-            sf::RectangleShape ProgressBackground;
-            ProgressBackground.setFillColor(sf::Color::White);
-            ProgressBackground.setOutlineThickness(2);
-            ProgressBackground.setOutlineColor(sf::Color::Red);
-            ProgressBackground.setSize(sf::Vector2f(length, width));
-            ProgressBackground.move(300.f, y);
+            for(int i = 0; i < main_b->m_nbParking; i++){
+                sf::Text text("Parking " + to_string(i+1), font, 50);
+                int y = i*50;
+                text.move(0.f, y);
+                
+                int length = 300;
+                int width = 50;
+                float progress_scale = length/tab_capacite[i];
 
-            sf::RectangleShape ProgressBar;
-            ProgressBar.setFillColor(sf::Color::Red);
-            ProgressBar.setOutlineThickness(2);
-            ProgressBar.setOutlineColor(sf::Color::Red);
-            ProgressBar.setSize(sf::Vector2f(stoi(listeParking[i]->getRemplissage()) * progress_scale, width));
-            ProgressBar.move(300.f, y);
+                sf::RectangleShape ProgressBackground;
+                ProgressBackground.setFillColor(sf::Color::White);
+                ProgressBackground.setOutlineThickness(2);
+                ProgressBackground.setOutlineColor(sf::Color::Red);
+                ProgressBackground.setSize(sf::Vector2f(length, width));
+                ProgressBackground.move(300.f, y);
 
-            window.draw(ProgressBackground);
-            window.draw(ProgressBar);
-            window.draw(text);  
-        }
+                sf::RectangleShape ProgressBar;
+                ProgressBar.setFillColor(sf::Color::Red);
+                ProgressBar.setOutlineThickness(2);
+                ProgressBar.setOutlineColor(sf::Color::Red);
+                //cout << stoi(main_b->m_listeParking[i]->getRemplissage())  << " / " << length << endl;
+                ProgressBar.setSize(sf::Vector2f(stoi(main_b->m_listeParking[i]->getRemplissage()) * progress_scale, width));
+                ProgressBar.move(300.f, y);
 
-        window.display();        
+                window.draw(ProgressBackground);
+                window.draw(ProgressBar);
+                window.draw(text);  
+            }
+
+            window.display(); 
+        }       
     }
 
     return 0;
